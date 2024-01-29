713ed764a1fb5cf1f694c13435ee98eef3ea4828
revione <revione@outlook.com>
2024-01-29 17:28:05 +0100

    Collections group logic

--- app/components/Collections.tsx
+++ app/components/Collections.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ChangeEvent, useMemo, useState } from "react";
+import { ChangeEvent, useEffect, useMemo, useState } from "react";
 import { CollectionData, GroupBySelect, Stage, State } from "@/app/types";
 
 import Collection from "./Collection";
@@ -7,7 +7,7 @@ import SelectsFilter from "./filters/SelectsFilter";
 import BooleanFilter from "./filters/BooleanFilter";
 import SearchFilters from "./filters/SerchFilter";
 
-import { Box, Container, Grid } from "@mui/material";
+import { Box, Container, Grid, Typography } from "@mui/material";
 
 export default function Collections({
   collections,
@@ -21,8 +21,10 @@ export default function Collections({
   });
 
   const [parentHubNamePortfolio, setParentHubNamePortfolio] = useState(true);
-
   const [searchText, setSearchText] = useState("");
+  const [groupsCollections, setGroups] = useState<
+    Record<string, CollectionData[]>
+  >({});
 
   const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
     setSearchText(event.target.value);
@@ -51,6 +53,27 @@ export default function Collections({
     });
   }, [collections, parentHubNamePortfolio, selects, searchText]);
 
+  useEffect(() => {
+    const groupBy = selects.groupBy;
+
+    if (groupBy !== "none") {
+      const collectionGroups = filteredCollections.reduce(
+        (acc, collection) => {
+          const key =
+            groupBy === GroupBySelect.state
+              ? collection.state
+              : collection.stage;
+          if (!acc[key]) acc[key] = [];
+          acc[key].push(collection);
+          return acc;
+        },
+        {} as Record<string, CollectionData[]>
+      );
+
+      setGroups(collectionGroups);
+    }
+  }, [filteredCollections, selects.groupBy]);
+
   return (
     <Container sx={{ marginY: "1rem" }}>
       <Box
@@ -71,15 +94,35 @@ export default function Collections({
           handleSearchChange={handleSearchChange}
         />
       </Box>
-      <Box>
-        <Grid container spacing={2}>
-          {filteredCollections.map((collection) => (
-            <Grid item key={collection.uuid} xs={12} sm={6} md={6} lg={4}>
-              <Collection collectionData={collection} />
-            </Grid>
+
+      {selects.groupBy !== "none" ? (
+        <Box>
+          {Object.keys(groupsCollections).map((key) => (
+            <Box key={key}>
+              <Typography variant="h3">{key}</Typography>
+              <CollectionsGrid collections={groupsCollections[key]} />
+            </Box>
           ))}
-        </Grid>
-      </Box>
+        </Box>
+      ) : (
+        <CollectionsGrid collections={filteredCollections} />
+      )}
     </Container>
   );
 }
+
+const CollectionsGrid = ({
+  collections,
+}: {
+  collections: CollectionData[];
+}) => (
+  <Box>
+    <Grid container spacing={2}>
+      {collections.map((collection) => (
+        <Grid item key={collection.uuid} xs={12} sm={6} md={6} lg={4}>
+          <Collection collectionData={collection} />
+        </Grid>
+      ))}
+    </Grid>
+  </Box>
+);
